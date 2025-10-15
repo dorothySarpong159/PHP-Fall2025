1de9d0175d25ea8ef62968813b481863fa27376f
dorothySarpong159 <200624161@student.georgianc.on.ca>
2025-10-14 20:29:15 -0400

    Add files via upload

---
+++ assignment3/templates/footer.php
@@ -0,0 +1,6 @@
+</main>
+    <footer>
+        <small>&copy; <?php echo date('Y'); ?> Assignment 3</small>
+    </footer>
+    </body>
+</html>
---
+++ assignment3/templates/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="description" content="<?php echo $pageDesc ?>">
+        <title><?php echo $pageTitle ?> </title>
+        
+        <meta name="robots" content="noindex,nofollow">
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
+        
+        <link rel="stylesheet" href="./css/style.css">
+    </head>
+    <body>
+        <header class="main-header">
+            <nav class="main-nav">
+                <div class="site-title">
+                    <a >Profile App</a>
+                </div>
+                <ul class="nav-links">
+                    <li><a href="index.php">View Profiles</a></li>
+                    <li><a href="create.php">Creat  Profile</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main class="content-area"> 
